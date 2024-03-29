6cf3e09f8b7bb65370694dc9a8ebed4721495a34
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2024-03-29 16:38:55 +0700

    feat: app database and inject

---
+++ app/src/main/java/com/ath/bondoman/data/database/AppDatabase.kt
@@ -0,0 +1,15 @@
+package com.ath.bondoman.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.ath.bondoman.model.TransactionCategoryConverter
+import com.ath.bondoman.model.Transaction
+import com.ath.bondoman.model.dao.TransactionDao
+
+
+@Database(entities = [Transaction::class], version = 1)
+@TypeConverters(TransactionCategoryConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun transactionDao(): TransactionDao
+}
