82c42cec924bba10a7a8fdd133c60f0e5aa020ea
chaerla <chelgchen@gmail.com>
2024-03-30 03:38:48 +0700

    feat: add check token on auth client

--- app/src/main/java/com/ath/bondoman/api/AuthClient.kt
+++ app/src/main/java/com/ath/bondoman/api/AuthClient.kt
@@ -2,8 +2,10 @@ package com.ath.bondoman.api
 
 import com.ath.bondoman.model.dto.LoginPayload
 import com.ath.bondoman.model.dto.LoginResponse
+import com.ath.bondoman.model.dto.TokenResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthClient {
@@ -11,4 +13,7 @@ interface AuthClient {
     suspend fun login(
         @Body auth: LoginPayload,
     ): Response<LoginResponse>
+
+    @POST("auth/token")
+    suspend fun verifyJwt(@Header("Authorization") token: String): Response <TokenResponse>
 }
