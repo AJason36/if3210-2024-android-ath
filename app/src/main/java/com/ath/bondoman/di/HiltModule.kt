82e89702a00ff465b6813d60c01c372bd8bb62ab
chaerla <chelgchen@gmail.com>
2024-03-30 03:38:20 +0700

    chore: updated hilt module and application

--- app/src/main/java/com/ath/bondoman/di/HiltModule.kt
+++ app/src/main/java/com/ath/bondoman/di/HiltModule.kt
@@ -9,13 +9,15 @@ import com.ath.bondoman.repository.TransactionRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.components.ViewModelComponent
 import retrofit2.Retrofit
 
 
 // put dependencies whose lifecycle is tied to a view model
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(ViewModelComponent::class, ServiceComponent::class, FragmentComponent::class)
 class HiltModule {
     @Provides
     fun provideTokenRepository(tokenDataStore: TokenDataStore): TokenRepository = TokenRepository(tokenDataStore)
