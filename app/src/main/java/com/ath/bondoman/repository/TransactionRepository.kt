9267383a147a7a137e208a2e3cc47e0cbd25712d
Alexander Jason <alexanderjason526@gmail.com>
2024-03-31 14:57:32 +0700

    feat: add get income and expenditure

--- app/src/main/java/com/ath/bondoman/repository/TransactionRepository.kt
+++ app/src/main/java/com/ath/bondoman/repository/TransactionRepository.kt
@@ -1,16 +1,23 @@
 package com.ath.bondoman.repository
 
-import androidx.lifecycle.LiveData
 import com.ath.bondoman.model.Transaction
 import com.ath.bondoman.model.dao.TransactionDao
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlinx.coroutines.flow.Flow
 
 @Singleton
 class TransactionRepository @Inject constructor(private val transactionDao: TransactionDao) {
     fun getAll(): Flow<List<Transaction>> {
         return transactionDao.getAll()
+    }
+
+    fun getAllIncome(): Flow<Double>{
+        return transactionDao.getAllIncome()
+    }
+
+    fun getAllExpenditure(): Flow<Double>{
+        return transactionDao.getAllExpenditure()
     }
 
     suspend fun insert(transaction: Transaction) : Long {
