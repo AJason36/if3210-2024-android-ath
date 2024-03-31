bd88646005b168941b2db74bdd8fb9fe5c681ac6
Alexander Jason <alexanderjason526@gmail.com>
2024-03-31 15:04:08 +0700

    feat: add chart view model

--- app/src/main/java/com/ath/bondoman/viewmodel/ChartViewModel.kt
+++ app/src/main/java/com/ath/bondoman/viewmodel/ChartViewModel.kt
@@ -3,11 +3,19 @@ package com.ath.bondoman.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.ath.bondoman.repository.TransactionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
-class ChartViewModel : ViewModel() {
+@HiltViewModel
+class ChartViewModel @Inject constructor(private val repository: TransactionRepository) : ViewModel(){
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is chart Fragment"
     }
     val text: LiveData<String> = _text
+    val allIncome: LiveData<Double> = repository.getAllIncome().asLiveData(Dispatchers.IO)
+    val allExpenditure: LiveData<Double> = repository.getAllExpenditure().asLiveData(Dispatchers.IO)
 }
